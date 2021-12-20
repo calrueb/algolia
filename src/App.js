7eae79832fd241cea758aa9e14cf39f1da7a1875
Cal Rueb <cal@calrueb.com>
2021-12-20 13:16:26 -0600

    Basic search working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const searchClient = algoliasearch(
+  "SH1EB5XM7P",
+  "8b2904a7e315e5e4c4503d5dd0236f5e"
+);
+
+const App = () => (
+  <InstantSearch indexName="test_billionaires" searchClient={searchClient}>
+    <SearchBox />
+    <Hits />
+  </InstantSearch>
+);
 
 export default App;
